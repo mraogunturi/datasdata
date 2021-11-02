b3aee36e434519898d97b880803c5eaa419be852
mraogunturi <mraogunturi@gmail.com>
2021-11-01 21:41:04 -0400

    logs

--- datas/datasdata/datasdata/datasdata/views.py
+++ datas/datasdata/datasdata/datasdata/views.py
@@ -1,12 +1,12 @@
 import logging
 from django.http import HttpResponse
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__file__)
 
 
 def hello(request,name):
-    logger.debug("Hey there it works!!")
-    logger.info("Hey there it works!!")
-    logger.warning("Hey there it works!!")
-    logger.error("Hey there it works!!")
-    logger.critical("Hey there it works!!")
+    logger.debug("Hey there it debug!!")
+    logger.info("Hey there it info!!")
+    logger.warning("Hey there it warning!!")
+    logger.error("Hey there it error!!")
+    logger.critical("Hey there it critical!!")
     return HttpResponse("Hello "+ name)
