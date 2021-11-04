bde176083693edfc8d9592e90a6111bb1737e872
mraogunturi <75759705+mraogunturi@users.noreply.github.com>
2021-11-04 11:17:49 -0400

    Update settings.py

--- datas/datasdata/datasdata/datasdata/settings.py
+++ datas/datasdata/datasdata/datasdata/settings.py
@@ -76,24 +76,24 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'datasdata',
-        'USER': 'postgres',
-        'PASSWORD': 'Mahesh',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'datasdata',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Mahesh',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
